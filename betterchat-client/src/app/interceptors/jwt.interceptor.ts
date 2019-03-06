6e0c06ca0111da9848ce502219d9eaeceb08ee18
zerju <jure.zerak@gmail.com>
2019-03-06 13:40:56 +0100

    Dont send jwt when not logged in

--- betterchat-client/src/app/interceptors/jwt.interceptor.ts
+++ betterchat-client/src/app/interceptors/jwt.interceptor.ts
@@ -1,3 +1,4 @@
+import { environment } from './../../environments/environment';
 import { AppState } from './../core/state/app.state';
 import { AuthState, AuthStateModel } from './../core/state/auth.state';
 import { Injectable } from '@angular/core';
@@ -12,15 +13,17 @@ import {
 import { Observable } from 'rxjs';
 import { Store } from '@ngxs/store';
 
-const HEADERS = new HttpHeaders({
-  bearer: 'some token'
-});
+const API_URL = environment.apiUrl;
+const PATHS_WITHOUT_TOKEN = [`${API_URL}/auth/login`, `${API_URL}/auth/register`];
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
   constructor(private authState: AuthState, private _store: Store) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if (PATHS_WITHOUT_TOKEN.includes(req.url)) {
+      return next.handle(req);
+    }
     const token = this._store.selectSnapshot<string>((state: AppState) => {
       return state.auth.user.jwtToken;
     });
